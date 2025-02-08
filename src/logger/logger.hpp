96d3f786907b4dc2442692e1afac67fe260b4a54
S-Pladison <spladison@gmail.com>
2025-02-08 04:46:13 +0300

    Немного переделана логика работы с функциями Logger-а

--- src/logger/logger.hpp
+++ src/logger/logger.hpp
@@ -3,6 +3,7 @@
 
 #include <stdexcept>
 #include <string>
+#include <sys/types.h>
 
 #include "SDL_log.h"
 #include "fmt/base.h"
@@ -13,73 +14,13 @@ namespace gb::Logger {
 /**
  * @brief Перечисление уровней логирования.
  */
-enum class Level {
+enum class Level : u_short {
   kDebug,   //< Отладочная информация, не влияющие на работу программы
   kInfo,    //< Сообщения, не влияющие на работу программы
   kWarning, //< Предупреждения, которые могут привести к непредвиденному поведению
   kError,   //< Ошибки, которые могут привести к непредвиденному поведению
   kFatal    //< Критические ошибки, прерывающие выполнение программы
 };
-
-/**
- * @brief Функция для вывода логов на уровне DEBUG.
- * 
- * @param fmt Сообщение для вывода.
- * @param args Аргументы для форматирования сообщения.
- */
-template<typename... Args>
-void Debug(const std::string& fmt, Args&&... args) {
-  auto msg = fmt::format(fmt::runtime(fmt), args...);
-  SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
-}
-
-/**
- * @brief Функция для вывода логов на уровне INFO.
- * 
- * @param fmt Сообщение для вывода.
- * @param args Аргументы для форматирования сообщения.
- */
-template<typename... Args>
-void Info(const std::string& fmt, Args&&... args) {
-  auto msg = fmt::format(fmt::runtime(fmt), args...);
-  SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
-}
-
-/**
- * @brief Функция для вывода логов на уровне WARN.
- * 
- * @param fmt Сообщение для вывода.
- * @param args Аргументы для форматирования сообщения.
- */
-template<typename... Args>
-void Warn(const std::string& fmt, Args&&... args) {
-  auto msg = fmt::format(fmt::runtime(fmt), args...);
-  SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
-}
-
-/**
- * @brief Функция для вывода логов на уровне ERROR.
- * 
- * @param fmt Сообщение для вывода.
- * @param args Аргументы для форматирования сообщения.
- */
-template<typename... Args>
-void Error(const std::string& fmt, Args&&... args) {
-  auto msg = fmt::format(fmt::runtime(fmt), args...);
-  SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
-}
-
-/**
- * @brief Функция для вывода логов на уровне FATAL.
- * 
- * @param fmt Сообщение для вывода.
- * @param args Аргументы для форматирования сообщения.
- */
-template<typename... Args>
-void Fatal(const std::string& fmt, Args&&... args) {
-  auto msg = fmt::format(fmt::runtime(fmt), args...);
-  SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
-}
 
 /**
  * @brief Функция для вывода логов.
@@ -89,29 +30,85 @@ void Fatal(const std::string& fmt, Args&&... args) {
  * @param args Аргументы для форматирования сообщения.
  * @throw std::logic_error Если передан недопустимый уровень логирования.
  */
-template<typename... Args>
-void Log(Level level, const std::string& fmt, Args&&... args) {
-  switch (level) {
-    case Level::kDebug:
-      Debug(fmt, args...);
-      break;
-    case Level::kInfo:
-      Info(fmt, args...);
-      break;
-    case Level::kWarning:
-      Warn(fmt, args...);
-      break;
-    case Level::kError:
-      Error(fmt, args...);
-      break;
-    case Level::kFatal:
-      Fatal(fmt, args...);
-      break;
-    default:
-      throw std::logic_error(
-        fmt::format("Недопустимый уровень логирования: {}", static_cast<int>(level))
-      );
+template<Level Level, typename... Args>
+void Log(const std::string& fmt, Args&&... args) {
+  auto msg = fmt::format(fmt::runtime(fmt), args...);
+
+  if constexpr (Level == Level::kDebug) {
+    SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
   }
+  else if constexpr (Level == Level::kInfo) {
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
+  }
+  else if constexpr (Level == Level::kWarning) {
+    SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
+  }
+  else if constexpr (Level == Level::kError) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
+  }
+  else if constexpr (Level == Level::kFatal) {
+    SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.c_str());
+  }
+  else {
+    throw std::logic_error(
+      fmt::format("Нереализованный уровень логирования: {}", static_cast<u_short>(Level))
+    );
+  }
+ }
+
+/**
+ * @brief Функция для вывода логов на уровне DEBUG.
+ * 
+ * @param fmt Сообщение для вывода.
+ * @param args Аргументы для форматирования сообщения.
+ */
+template<typename... Args>
+inline void Debug(const std::string& fmt, Args&&... args) {
+  Log<Level::kDebug>(fmt, args...);
+}
+
+/**
+ * @brief Функция для вывода логов на уровне INFO.
+ * 
+ * @param fmt Сообщение для вывода.
+ * @param args Аргументы для форматирования сообщения.
+ */
+template<typename... Args>
+inline void Info(const std::string& fmt, Args&&... args) {
+  Log<Level::kInfo>(fmt, args...);
+}
+
+/**
+ * @brief Функция для вывода логов на уровне WARN.
+ * 
+ * @param fmt Сообщение для вывода.
+ * @param args Аргументы для форматирования сообщения.
+ */
+template<typename... Args>
+inline void Warn(const std::string& fmt, Args&&... args) {
+  Log<Level::kWarning>(fmt, args...);
+}
+
+/**
+ * @brief Функция для вывода логов на уровне ERROR.
+ * 
+ * @param fmt Сообщение для вывода.
+ * @param args Аргументы для форматирования сообщения.
+ */
+template<typename... Args>
+inline void Error(const std::string& fmt, Args&&... args) {
+  Log<Level::kError>(fmt, args...);
+}
+
+/**
+ * @brief Функция для вывода логов на уровне FATAL.
+ * 
+ * @param fmt Сообщение для вывода.
+ * @param args Аргументы для форматирования сообщения.
+ */
+template<typename... Args>
+inline void Fatal(const std::string& fmt, Args&&... args) {
+  Log<Level::kFatal>(fmt, args...);
 }
 
 } // namespace gb::Logger
